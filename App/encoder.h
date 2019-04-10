7b7c18420f815830b96b2f19d672575aab83b2b6
Kuiooo <lmmlobb@gmail.com>
2019-04-10 20:19:56 +0800

    Init
    Signed-off-by: Kuiooo <lmmlobb@gmail.com>

--- App/encoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ENCODER_H
-#define _ENCODER_H
-#include "sys.h"
-void encoder_init(void);
-#endif
-
--- SYSTEM/usart.h
+++
@@ -1,9 +0,0 @@
-#ifndef _USART_H
-#define _USART_H
-#include "stm32f4xx.h"
-
-void Usart_Init(void);
-void USARTSendFloat(float data, unsigned char pos);
-void USARTSendInt(int data,unsigned char pos);
-
-#endif
